a99199e95b5eb9fa091bc0045d9d21bcb66f4851
Bhautik Hirapara <20dit025@charusat.edu.in>
2023-03-04 15:25:12 +0530

    2nd commit
    first time data fetch from api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{width:'18rem'}}>
-            <img src={imgURL} className="card-img-top" alt="..."/>
+            <img src={!imgURL ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0DbhG4bav0M98FVHKzl-_o6Z9KsiqMqhPeg&usqp=CAU" : imgURL} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <a href={newsURL} target="_blank" className="btn btn-primary">Go somewhere</a>
+                <a href={newsURL} target="_blank" rel="noreferrer" className="btn btn-primary">Go somewhere</a>
             </div>
             </div>
       </div>
